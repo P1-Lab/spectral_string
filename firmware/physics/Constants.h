156d3186281d41eee5a69dc59d752b764f24ab55
P1-Lab <principal.p1.lab@proton.me>
2026-05-02 09:23:11 -0400

    Create Constants.h

---
+++ firmware/physics/Constants.h
@@ -0,0 +1,7 @@
+#pragma once
+
+constexpr float DEFAULT_TENSION = 1.0f;
+
+// Physical constants (spectral mapping core)
+constexpr float SPEED_OF_LIGHT = 299792458.0f;
+constexpr float PHI = 1.6180339887f;
